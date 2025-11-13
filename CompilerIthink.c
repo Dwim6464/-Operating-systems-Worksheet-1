d607e1dfce83a39208903a3cbb9847c3d7c1e7e9
Dwim6464 <dwmdude1@gmail.com>
2025-11-13 10:01:07 +0000

    Create CompilerIthink.c

---
+++ CompilerIthink.c
@@ -0,0 +1,9 @@
+// this might be a driver I can't remember and it doesn't matter
+// hopefully
+#include <stdio.h>
+int main() {
+
+    printf("Hello, Hopefully this should compile something");
+
+    return 0; //This statement indicates that the program ended successfully.
+}
