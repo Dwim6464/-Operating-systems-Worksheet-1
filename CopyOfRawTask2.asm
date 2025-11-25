93938c70e282da9bd4429747d7ff860d2e1a48fa
Dwim6464 <dwmdude1@gmail.com>
2025-11-25 18:42:07 +0000

    Create CopyOfRawTask2.asm

---
+++ CopyOfRawTask2.asm
@@ -0,0 +1,77 @@
+; This is a comment in assembly, hello
+; use the online compiler https://onecompiler.com/assembly/3uccjrhsa
+
+section .data
+    hello:      db 'Hello world!', 10 ; 'Hello world!' plus a linefeed character, which happens to be the decimal number 10
+    ; a linefeed character is like an enter which moves onto the next line
+    ; although it looks like the best way to get more spaces is to go , 100 you should actually do , 10, 10 as , 100 provides the letter d
+    helloLen:   equ $ - hello
+
+    askName:    db 'What is your name? ', 10
+    askNameLen: equ $ - askName
+
+    hey:        db 'Hey there ', 0
+    heyLen:     equ $ - hey
+
+    exclaim:    db '!', 10
+    exclaimLen: equ $ - exclaim
+
+section .bss
+    nameInput:  resb 64        ; buffer to store user's name (max 63 chars + newline)
+
+section .text
+    global _start
+
+_start:
+    ; Print "Hello world!"
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, hello
+    mov edx, helloLen
+    int 0x80
+
+    ; Ask for user's name
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, askName
+    mov edx, askNameLen
+    int 0x80
+
+    ; Read user input
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, nameInput
+    mov edx, 64
+    int 0x80
+    mov esi, eax             ; save number of bytes read
+
+    ; Print "Hey there "
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, hey
+    mov edx, heyLen
+    int 0x80
+
+    ; Print the name (user input contains the newline from pressing Enter)
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, nameInput
+    mov edx, esi
+    int 0x80
+
+    ; Print "!"
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, exclaim
+    mov edx, exclaimLen
+    
+
+
+    ;apparently don't use interrupts OR I MAY FAIL!!!!
+    ; Exit
+    int 0x80 ; Call the kernel
+    mov eax, 1 ; The system call for exit (sys_exit)
+    mov ebx, 0 ; Exit with return code of 0 (no error)
+    int 0x80 ; This is an interrupt that tells the kernel to perform the system call it just described
+    
+    ;This is my int 80h; to say this is the end of the code, make sure this is here so you know you got everything
