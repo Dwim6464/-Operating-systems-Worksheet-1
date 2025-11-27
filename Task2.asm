2c9425b6cc21538d76a80e8e3a1f0eb544959987
Dwim6464 <dwmdude1@gmail.com>
2025-11-27 03:25:20 +0000

    Update Task2.asm

--- Task2.asm
+++ Task2.asm
@@ -1,5 +1,4 @@
-; This is a comment in assembly, hello
-; use the online compiler https://onecompiler.com/assembly/3uccjrhsa
+%include "asm_io.inc"
 
 section .data
     hello:      db 'Hello world!', 10 ; 'Hello world!' plus a linefeed character, which happens to be the decimal number 10
@@ -20,58 +19,63 @@ section .bss
     nameInput:  resb 64        ; buffer to store user's name (max 63 chars + newline)
 
 section .text
-    global _start
+        global asm_main
+    asm_main:
+        mov eax,0
+        mov ebx,0
+        mov ecx,0
+        mov edx,0
+        pusha
 
-_start:
-    ; Print "Hello world!"
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, hello
-    mov edx, helloLen
-    int 0x80
+        ; Print "Hello world!"
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, hello
+        mov edx, helloLen
+        int 0x80
 
-    ; Ask for user's name
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, askName
-    mov edx, askNameLen
-    int 0x80
+        ; Ask for user's name
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, askName
+        mov edx, askNameLen
+        int 0x80
 
-    ; Read user input
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, nameInput
-    mov edx, 64
-    int 0x80
-    mov esi, eax             ; save number of bytes read
+        ; Read user input
+        mov eax, 3
+        mov ebx, 0
+        mov ecx, nameInput
+        mov edx, 64
+        int 0x80
+        mov esi, eax             ; save number of bytes read
 
-    ; Print "Hey there "
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, hey
-    mov edx, heyLen
-    int 0x80
+        ; Print "Hey there "
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, hey
+        mov edx, heyLen
+        int 0x80
 
-    ; Print the name (user input contains the newline from pressing Enter)
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, nameInput
-    mov edx, esi
-    int 0x80
+        ; Print the name (user input contains the newline from pressing Enter)
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, nameInput
+        mov edx, esi
+        int 0x80
 
-    ; Print "!"
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, exclaim
-    mov edx, exclaimLen
-    
-
-
-    ;apparently don't use interrupts OR I MAY FAIL!!!!
-    ; Exit
-    int 0x80 ; Call the kernel
-    mov eax, 1 ; The system call for exit (sys_exit)
-    mov ebx, 0 ; Exit with return code of 0 (no error)
-    int 0x80 ; This is an interrupt that tells the kernel to perform the system call it just described
+        ; Print "!"
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, exclaim
+        mov edx, exclaimLen
+        
+        popa ;this pushes all the registers onto the stack then pops . ask if the code below counteracts that
+        mov eax,0 
+        mov ebx,0
+        mov ecx,0
+        mov edx,0
+        leave;initially this was done with an interrupt but I was told to avoid using interrupts where possible as 
+        ;it is less efficient for the CPU and slows down the program
+        ret ;short for return
     
     ;This is my int 80h; to say this is the end of the code, make sure this is here so you know you got everything
