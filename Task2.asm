334da8cabb7097e92aa8fba8cb84d8fd967d7f80
Dwim6464 <dwmdude1@gmail.com>
2025-12-10 11:48:49 +0000

    Improve user prompts and input handling in Task2.asm
    Refactor prompts and comments for clarity; add number input handling.

--- Task2.asm
+++ Task2.asm
@@ -1,13 +1,22 @@
 %include "asm_io.inc"
 
 section .data
-    hello:      db 'Hello world!', 10 ; 'Hello world!' plus a linefeed character, which happens to be the decimal number 10
+    hello:      db 'Hello world!', 10' ;Hello world!' plus a linefeed character, which happens to be the decimal number 10
     ; a linefeed character is like an enter which moves onto the next line
     ; although it looks like the best way to get more spaces is to go , 100 you should actually do , 10, 10 as , 100 provides the letter d
     helloLen:   equ $ - hello
 
-    askName:    db 'What is your name? ', 10
+    askName:    db 'What is your name? ', 10 ;db means define byte and saves one byte in memory for each character
     askNameLen: equ $ - askName
+
+    askNum:     db 'Enter a number between 50 and 100: ', 10
+    askNumLen:  equ $ - askNum
+
+    tooSmall:   db 'Error: number too small.', 10
+    tooSmallLen: equ $ - tooSmall
+
+    tooLarge:   db 'Error: number too large.', 10
+    tooLargeLen: equ $ - tooLarge
 
     hey:        db 'Hey there ', 0
     heyLen:     equ $ - hey
@@ -16,18 +25,19 @@ section .data
     exclaimLen: equ $ - exclaim
 
 section .bss
-    nameInput:  resb 64        ; buffer to store user's name (max 63 chars + newline)
+    nameInput:  resb 64  ; a buffer of 64 bytes to store user's name (max 63 chars + newline)
+    numInput:   resb 16
 
 section .text
         global asm_main
     asm_main:
-        mov eax,0
+        mov eax,0 ;initialize registers
         mov ebx,0
         mov ecx,0
         mov edx,0
         pusha
 
-        ; Print "Hello world!"
+        ; print "Hello world!"
         mov eax, 4
         mov ebx, 1
         mov ecx, hello
@@ -41,14 +51,51 @@ section .text
         mov edx, askNameLen
         int 0x80
 
-        ; Read user input
+        ; Read user name
         mov eax, 3
         mov ebx, 0
         mov ecx, nameInput
         mov edx, 64
         int 0x80
-        mov esi, eax             ; save number of bytes read
+        mov esi, eax
 
+        ; ask for a number
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, askNum
+        mov edx, askNumLen
+        int 0x80
+
+        ; read the number input
+        mov eax, 3
+        mov ebx, 0
+        mov ecx, numInput
+        mov edx, 16
+        int 0x80
+
+        ; convert ASCII to a normal number
+        mov ecx, numInput
+        xor eax, eax
+    convert_loop:
+        mov bl, [ecx]
+        cmp bl, 10
+        je convert_done
+        sub bl, '0'
+        imul eax, eax, 10
+        add eax, ebx
+        inc ecx
+        jmp convert_loop
+    convert_done:
+        mov edi, eax ; store number in the registe edi
+
+        cmp edi, 50 ; check that the number is more than 50
+        jle number_small
+        cmp edi, 100 ; check that the number is less than 100
+        jge number_large
+
+        ; Print welcome line edi times
+        mov ebx, edi
+    print_loop:
         ; Print "Hey there "
         mov eax, 4
         mov ebx, 1
@@ -56,26 +103,46 @@ section .text
         mov edx, heyLen
         int 0x80
 
-        ; Print the name (user input contains the newline from pressing Enter)
+        ; Print name
         mov eax, 4
         mov ebx, 1
         mov ecx, nameInput
         mov edx, esi
         int 0x80
 
-        ; Print "!"
+        ; Print '!'
         mov eax, 4
         mov ebx, 1
         mov ecx, exclaim
         mov edx, exclaimLen
-        
-        popa ;this pushes all the registers onto the stack then pops . ask if the code below counteracts that
-        mov eax,0 
+        int 0x80
+
+        dec edi
+        jnz print_loop
+        jmp end_prog
+
+    number_small:
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, tooSmall
+        mov edx, tooSmallLen
+        int 0x80
+        jmp end_prog
+
+    number_large:
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, tooLarge
+        mov edx, tooLargeLen
+        int 0x80
+
+    end_prog:
+        popa ;this pushes all the registers onto the stack then pops. ask max if the code below counteracts that since it was his idea
+        mov eax,0
         mov ebx,0
         mov ecx,0
         mov edx,0
-        leave;initially this was done with an interrupt but I was told to avoid using interrupts where possible as 
+        leave ;initially this was done with an interrupt but I was told to avoid using interrupts where possible as 
         ;it is less efficient for the CPU and slows down the program
         ret ;short for return
-    
-    ;This is my int 80h; to say this is the end of the code, make sure this is here so you know you got everything
+
